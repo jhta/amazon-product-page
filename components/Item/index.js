7e83dada9ed0c68e7dd8a2a05ef17a89a486c5d3
jhta <jeisonhs93@gmail.com>
2018-07-26 16:40:08 -0500

    defining structure

--- components/Item/index.js
+++ components/Item/index.js
@@ -1,5 +1,46 @@
 import React from 'react'
 
-const Item = ({ title }) => <li>{ title }</li>
+const Meta = ({ reviewCreated, reviewId, productTitle, stars }) => (
+  <article>
+    <figure>
+      <img src='#' width='80' height='80' />
+    </figure>
+    <div>
+      <p>Date:</p>
+      <p>{reviewCreated}</p>
+    </div>
+    <div>
+      <p>Stars:</p>
+      <p>{ stars }</p>
+    </div>
+    <div>
+      <p>{ reviewId }</p>
+      <p>{ productTitle }</p>
+    </div>
+    <style jsx>{`
+      article {
+        display: flex;
+        border: 1px solid green;
+      }
+      `}</style>
+  </article>
+)
+
+const Item = (props) => (
+  <li>
+    <section>
+      <Meta {...props} />
+      <h2>{ props.title }</h2>
+      <p>{ props.content }</p>
+    </section>
+    <style jsx>{`
+      li {
+        border: 1px solid red;
+        padding: 1rem;
+        box-sizing: border-box;
+      }
+      `}</style>
+  </li>
+)
 
 export default Item
