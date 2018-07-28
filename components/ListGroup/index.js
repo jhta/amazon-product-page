5ae2d14b0ec2ace24d588f49a1f8bc6422176420
jhta <jeisonhs93@gmail.com>
2018-07-28 15:17:37 -0500

    add infinite-scroll component

--- components/ListGroup/index.js
+++ components/ListGroup/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import List from 'components/List'
+import InfiniteScroll from 'react-infinite-scroller'
 
 const ListGroup = ({ groups = [], order }) => (
-  <section>
-    {
-      groups.map(({ title, reviews }, index) => (
-        <article key={index}>
-          <h1>{title}</h1>
-          <List reviews={order === 'latest' ? reviews : reviews.slice().reverse()} />
-        </article>
-      ))
-    }
-  </section>
+  <InfiniteScroll
+    pageStart={0}
+    loadMore={() => { console.log('load more') }}
+    hasMore={true}
+    loader={<div>Loading...</div>}
+  >
+    <section>
+      {
+        groups.map(({ title, reviews }, index) => (
+          <article key={index}>
+            <h1>{title}</h1>
+            <List reviews={order === 'latest' ? reviews : reviews.slice().reverse()} />
+          </article>
+        ))
+      }
+    </section>
+  </InfiniteScroll>
 )
 
 export default ListGroup
